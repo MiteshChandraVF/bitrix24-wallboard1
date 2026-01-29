9af6480ad751ba73613c1543bf0e740fc0f9bf40
MiteshChandraVF <mitesh.chandra@vodafone.com.pg>
2026-01-30 00:30:31 +1000

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const WebSocket = require("ws");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const server = http.createServer(app);
